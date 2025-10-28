5a655a38f8fc1741226b2df3032d6ae9ca550d5d
Vikas <vikas.vp706@gmail.com>
2025-10-28 14:30:00 +0530

    Commit

---
+++ Day 15/references.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+void ref(){
+  string food = "Pizza";
+  string &meal = food;
+
+  cout << food << "\n";
+  cout << meal << "\n";
+  cout << &meal << endl;
+}
+
+int main(){
+  ref();
+  return 0;
+}

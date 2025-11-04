33a01ef7bb4ff6de91f838983b0a67389b715516
Vikas <vikas.vp706@gmail.com>
2025-11-04 10:07:36 +0530

    Initial commit, excluding .vscode folder

---
+++ Day 1/first_program.cpp
@@ -0,0 +1,12 @@
+// first_program.cpp
+// It was supposed to be Hello World but this is also fine.
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x, y, z;
+    cin >> x >> y >> z;
+    cout << "Hey " << x + y +z << endl;
+    return 0;
+}

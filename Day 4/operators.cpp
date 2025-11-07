f9cea5b0c64a578b1ea49a2d4c9cdf5b05799740
Vikas <vikas.vp706@gmail.com>
2025-11-07 12:10:43 +0530

    Day 4

--- Day 4/operators.cpp
+++ Day 4/operators.cpp
@@ -2,19 +2,46 @@
 using namespace std;
 
 // Function to perform all the arithemetic operations
-  void arithemetic(int x, int y){
-    cout << x + y << "\n";
-    cout << x - y << "\n";
-    cout << x * y << "\n";
-    cout << x / y << "\n";
-    cout << x % y << "\n";
-    cout << x++ << "\n";
-    cout << x-- << "\n";
-  }
+void arithemetic(int x, int y){
+  cout << x + y << "\n";
+  cout << x - y << "\n";
+  cout << x * y << "\n";
+  cout << x / y << "\n";
+  cout << x % y << "\n";
+  cout << x++ << "\n";
+  cout << x-- << "\n";
+}
 
+// Function to perform all the assignment operations
+void assignment(){
+  int a;
+  a = 10;
+  cout << a << "\n";
+  a += 3; // a = a + 3
+  cout << a << "\n";
+  a -= 3; // a = a - 3
+  cout << a << "\n";
+  a *= 3; // a = a * 3
+  cout << a << "\n";
+  a /= 3; // a = a / 3
+  cout << a << "\n";
+  a %= 3; // a = a % 3
+  cout << a << "\n";
+  a &= 3; // a = a & 3
+  cout << a << "\n";
+  a |= 3; // a = a | 3
+  cout << a << "\n";
+  a ^= 3; // a = a ^ 3
+  cout << a << "\n";
+  a >>= 3; // a = a >> 3
+  cout << a << "\n";
+  a <<= 3; // a = a << 3
+  cout << a << "\n";
+}
 
 int main(){
 
-  arithemetic(10,2);
+  // arithemetic(10,2);
+  assignment();
 
 }

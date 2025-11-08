e0221127641df02f850a5ab2a6508429b4c08d8a
Vikas <vikas.vp706@gmail.com>
2025-11-08 06:26:24 +0530

    Day 5

---
+++ Day 5/strings.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+void learningStrings(){
+  // When creating a string type variable we must use the keyword "string"
+  string greeting = "Hello and Welcome!";
+  cout << greeting <<"\n";
+
+  string firstName = "James ";
+  string lastName = "Bond";
+  string fullName = firstName + lastName;
+  cout << fullName << "\n";
+  string fullName1 = firstName.append(lastName);
+  cout << fullName1 << "\n";
+
+  // String Functions
+  string a = "Hello ";
+  string b = "World!";
+  cout << a.append(b) << "\n";
+  cout << a.at(1) << "\n";
+  cout << a.back() << "\n";
+  cout << *a.begin() << "\n";
+  cout << a.c_str() << "\n";
+  // a.clear(); // Used to clear a string, vector etc. Return is void hence cout doesnt work.
+  cout << a.compare(b) << "\n";
+  // cout << a.copy() << "\n";
+
+}
+
+int main(){
+
+  learningStrings();
+}

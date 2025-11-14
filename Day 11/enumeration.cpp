d474c44382e473097a2a470468720237db8e8cb0
Vikas <vikas.vp706@gmail.com>
2025-11-14 12:19:03 +0530

    Day 11

--- Day 11/enumeration.cpp
+++ Day 11/enumeration.cpp
@@ -1,8 +1,16 @@
 #include<iostream>
 using namespace std;
 
+enum class Volume {
+  LOW,
+  MEDIUM,
+  HIGH,
+  HIGHER
+};
 
 
 int main(){
+  Volume v = Volume::HIGHER;
+  cout<< static_cast<int>(v);
   return 0;
 }

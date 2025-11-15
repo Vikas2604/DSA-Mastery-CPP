ec39d05c5440d3c84451e3365ad25e82b3e84a38
Vikas <vikas.vp706@gmail.com>
2025-11-15 20:57:52 +0530

    Day 12

--- Day 12/references.cpp
+++ Day 12/references.cpp
@@ -1,8 +1,27 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
+void reffering(){
+  string food = "Pizza";
+  string &meal = food;
 
+  cout << food << "\n";
+  cout << meal << "\n";
+
+  meal = "Burger";
+
+  cout << food << "\n";
+  cout << meal << endl;
+}
+
+void memoryAddress(){
+  string food = "Pizza";
+  cout << &food << endl;
+}
 
 int main(){
+  // reffering();
+  memoryAddress();xx 
   return 0;
 }
